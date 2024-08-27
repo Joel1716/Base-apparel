7d8cd06e26e8f9c2a688e43bb44c8f2a0382f409
Joel Ohikhena <122086681+Joel1716@users.noreply.github.com>
2024-08-27 19:07:23 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const inputs = document.querySelector("input");
+const buttonEl = document.querySelector("button");
+buttonEl.addEventListener("click", clicked);
+function clicked() {
+  if (inputs.validity.valid) {
+    inputs.value = "";
+    inputs.classList.remove("addInput");
+    document.querySelector(".js-display").innerHTML = "";
+  } else {
+    document.querySelector(".js-display").innerHTML =
+      "Please provide a valid email!";
+    // const errorImg = document.createElement("img");
+    // errorImg.src = "./images/icon-error.svg";
+    inputs.classList.add("addInput");
+  }
+}
+/// When you click Enter
+inputs.addEventListener("keydown", keyClick);
+function keyClick(event) {
+  if (event.key === "Enter") {
+    clicked();
+  }
+}
